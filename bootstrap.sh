76aabbe03cac4c0d0a1ce46b90a965a7f31770cd
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-06-29 21:35:33 +0300

    Platform drivers moved into dedicated repos

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,17 @@
 # | BASH : Modifying Shell Behaviour
 # |    (https://www.gnu.org/software/bash/manual)
 # +----------------------------------------------------------+
-# Treat unset variables and parameters other than the special 
-# parameters ‘@’ or ‘*’ as an error when performing parameter 
-# expansion. An error message will be written to the standard 
+# Treat unset variables and parameters other than the special
+# parameters ‘@’ or ‘*’ as an error when performing parameter
+# expansion. An error message will be written to the standard
 # error, and a non-interactive shell will exit.
 set -o nounset
 
 # Exit immediately if a pipeline returns a non-zero status.
 set -o errexit
 
-# If set, the return value of a pipeline is the value of the 
-# last (rightmost) command to exit with a non-zero status, or 
+# If set, the return value of a pipeline is the value of the
+# last (rightmost) command to exit with a non-zero status, or
 # zero if all commands in the pipeline exit successfully.
 set -o pipefail
 
@@ -32,4 +32,5 @@ sudo apt-get -y install g++-5;
 sudo apt-get -y install gcc-arm-embedded
 
 # Export to tell cmake which native compilers to use.
+# TODO: Migrate to GCC 7 or newer
 export CXX="g++-5" CC="gcc-5";
