653286cb12db5bfcec4ddbd7bcd23facddc3d8b5
doc-upload <doc-upload@uavcan.org>
2019-06-15 07:54:27 -0700

    Fixing cmake build

--- cmake/toolchains/gcc-native.cmake
+++ cmake/toolchains/gcc-native.cmake
@@ -3,6 +3,6 @@
 #
 # Toolchain for using gcc on what-ever-platform-this-is (aka "native").
 #
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
-set(CMAKE_ASM_COMPILER gcc)
+set(CMAKE_C_COMPILER gcc CACHE FILEPATH "C compiler")
+set(CMAKE_CXX_COMPILER g++ CACHE FILEPATH "C++ compiler")
+set(CMAKE_ASM_COMPILER gcc CACHE FILEPATH "assembler")
