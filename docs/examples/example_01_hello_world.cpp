a70076e132bb16e0de18f1f74ea3baf943a28f8b
Scott Dixon <dixonsco@amazon.com>
2023-06-07 11:40:23 -0700

    integrating nunavut

--- docs/examples/example_01_hello_world.cpp
+++ docs/examples/example_01_hello_world.cpp
@@ -14,11 +14,28 @@
 
 #include <gtest/gtest.h>
 
+#include "cetl/pf17/sys/memory_resource.hpp"
+#include "cetl/pf17/byte.hpp"
+#include "cetl/variable_length_array.hpp"
+
+#include "example/foo_1_0.hpp"
+
+using byte = cetl::pf17::byte;
+using monotonic_buffer_resource = cetl::pf17::pmr::deviant::basic_monotonic_buffer_resource;
+template<typename T>
+using polymorphic_allocator = cetl::pf17::pmr::polymorphic_allocator<T>;
+
 TEST(example_01_hello_world, snippit_0)
 {
 //! [snippit_0]
     // to include in doxygen just do:
     // @snippet{trimleft} example_01_hello_world.cpp snippit_0
     std::cout << "Hello World" << std::endl;
+
+    byte storage[10];
+    monotonic_buffer_resource mr{storage, sizeof(storage)};
+    polymorphic_allocator<int> alloc(&mr);
+    cetl::VariableLengthArray<int, polymorphic_allocator<int>> vla{alloc};
+
 //! [snippit_0]
 }
