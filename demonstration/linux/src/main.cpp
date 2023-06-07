b7b71259fff79ff09d16d4e43b28ab475888423b
Scott Dixon <dixonsco@amazon.com>
2023-06-07 11:40:23 -0700

    Remove utils.hpp
    Removing a use of std::string and hoisting a utility up into the main library.

--- demonstration/linux/src/main.cpp
+++ demonstration/linux/src/main.cpp
@@ -1,6 +1,10 @@
-#include "posix/libcyphal/utils.hpp"
+
+#include "posix/libcyphal/udp.hpp"
+
+libcyphal::UDPBroadcaster udp_broadcaster_{libcyphal::transport::ip::v4::Address::addressFromString("127.0.0.1"), 127};
 
 int main()
 {
-    return 0;
+    const libcyphal::Status result = udp_broadcaster_.initialize();
+    return (result.isSuccess() ? 0 : -1);
 }
--- libcyphal/include/libcyphal/transport/ip/v4/address.hpp
+++ libcyphal/include/libcyphal/transport/ip/v4/address.hpp
@@ -1,11 +1,18 @@
-/// @copyright Copyright Amazon.com Inc. and its affiliates. All Rights Reserved.
 /// @file
-/// A generic class representation of an IPv4 Address
+/// IPv4 Address class and utilities.
+///
+/// @copyright
+/// Copyright (C) OpenCyphal Development Team  <opencyphal.org>
+/// Copyright Amazon.com Inc. or its affiliates.
+/// SPDX-License-Identifier: MIT
+///
 
 #ifndef LIBCYPHAL_TRANSPORT_IP_V4_ADDRESS_HPP_INCLUDED
 #define LIBCYPHAL_TRANSPORT_IP_V4_ADDRESS_HPP_INCLUDED
 
 #include <cstdint>
+#include <algorithm>
+#include <cstring>
 
 namespace libcyphal
 {
@@ -19,19 +26,19 @@ namespace v4
 using Octet       = std::uint8_t;
 using AddressType = std::uint32_t;
 
-/// @brief The Address class represents an IPv4 Address
-/// The Address class provides functionality for storing, retrieve, and manipulating IPv4 addressess.
+/// The Address class represents an IPv4 Address.
+/// The Address class provides functionality for storing, retrieve, and manipulating IPv4 addresses.
 /// It supports the IPv4 format.
 class Address final
 {
 public:
-    /// @brief Address Default constructor
+    /// Address Default constructor
     constexpr Address() noexcept
         : Address(0u, 0u, 0u, 0u)
     {
     }
 
-    /// @brief Explicit Parameter Constructor as single unsigned 32 bit value (chaining)
+    /// Explicit Parameter Constructor as single unsigned 32 bit value (chaining)
     /// @param[in] address The 32-bit representation of the IPv4 Address
     constexpr explicit Address(AddressType address) noexcept
         : Address{Octet((address & 0xFF000000u) >> 24u),
@@ -41,7 +48,7 @@ public:
     {
     }
 
-    /// @brief Explicit Parameter Constructor as classical four part address
+    /// Explicit Parameter Constructor as classical four part address
     /// @note For A.B.C.D address instantiate in the same order as {A,B,C,D};
     /// @param[in] a The left most octet of the IPv4 Address read from left to right as a string
     /// @param[in] b The second to the left most octet of the IPv4 Address read from left to right as a string
@@ -55,17 +62,17 @@ public:
     {
     }
 
-    /// @brief Address Destructor
+    /// Address Destructor
     ~Address() noexcept = default;
 
-    /// @brief Copy Constructor for Address
+    /// Copy Constructor for Address
     /// @param[in] other Address to copy from
     constexpr Address(const Address& other) noexcept
         : Address(other.a_, other.b_, other.c_, other.d_)
     {
     }
 
-    /// @brief Move Constructor for Address
+    /// Move Constructor for Address
     /// @param[in] other Address to move from
     constexpr Address(Address&& other) noexcept
         : d_{other.d_}
@@ -79,7 +86,7 @@ public:
         other.d_ = 0;
     }
 
-    /// @brief Copy Assignment Operator for Address
+    /// Copy Assignment Operator for Address
     /// @param[in] other Address to copy from
     /// @return Updated address if different
     constexpr Address& operator=(const Address& other) noexcept
@@ -94,7 +101,7 @@ public:
         return (*this);
     }
 
-    /// @brief Move Assignment Operator for Address
+    /// Move Assignment Operator for Address
     /// @param[in] other Address to copy from
     /// @return Updated address if different
     constexpr Address& operator=(Address&& other) noexcept
@@ -113,7 +120,7 @@ public:
         return (*this);
     }
 
-    /// @brief Address Equality Check
+    /// Address Equality Check
     /// @param[in] other The Address class to compare against
     /// @return Whether the Addresses being compared are equal
     constexpr bool operator==(const Address& other) const noexcept
@@ -121,7 +128,7 @@ public:
         return (AddressType(*this) == AddressType(other));
     }
 
-    /// @brief Address Inequality Check
+    /// Address Inequality Check
     /// @param[in] other The Address class to compare against
     /// @return Whether the Addresses being compared are not equal
     constexpr bool operator!=(const Address& other) const noexcept
@@ -129,39 +136,114 @@ public:
         return not operator==(other);
     }
 
-    /// @brief Checks if address is a localhost address
+    /// Checks if address is a localhost address
     /// @return true when on the 127 subnet (localhost)
     constexpr bool isLocal() const noexcept
     {
         return (a_ == 0b0111'1111);
     }
 
-    /// @brief Checks if address is a multicast address
+    /// Checks if address is a multicast address
     /// @return true when a multicast address
     constexpr bool isMulticast() const noexcept
     {
         return ((a_ & 0xF0) == 0b1110'0000);
     }
 
-    /// @brief Operator to handle comparison with a 32-bit integer
+    /// Operator to handle comparison with a 32-bit integer
     /// @return 32-bit integer representation of the IPv4 Address
     constexpr explicit operator AddressType() const noexcept
     {
         return static_cast<AddressType>((a_ << 24u) | (b_ << 16u) | (c_ << 8u) | (d_ << 0u));
     }
 
-    /// @brief Checks if address is valid and not the default route or limited broadcast
+    /// Checks if address is valid and not the default route or limited broadcast
     /// @return true if the address is not the default route or the limited broadcast
     constexpr bool isValid() const noexcept
     {
         return operator!=(Address{0u, 0u, 0u, 0u}) and operator!=(Address{255u, 255u, 255u, 255u});
     }
 
-    /// @brief Converts Address into a 32-bit integer
+    /// Converts Address into a 32-bit integer
     /// @return Address represented as a 32-bit integer
     constexpr AddressType asInteger() const noexcept
     {
         return static_cast<AddressType>((a_ << 24u) | (b_ << 16u) | (c_ << 8u) | (d_ << 0u));
+    }
+
+    /// Converts a given string with (presumably) integers into an Octet.
+    /// @param[in] octet The string to convert into an Octet.
+    /// @param[in] octet_str_len The length of the octet argument.
+    /// @return The Octet representation of the given string. 0 is returned if octet is nullptr
+    ///         and 255 if the value is greater than 255. All other values between 0 and 255 are
+    ///         interpreted by strtoul.
+    static inline Octet octetFromBase10String(const char* octet, std::size_t octet_str_len)
+    {
+        static constexpr std::size_t MAX_OCTET_STR_LEN = 16u;
+        if (nullptr == octet)
+        {
+            return 0u;
+        }
+        std::size_t clamped_string_len = std::min(octet_str_len, MAX_OCTET_STR_LEN);
+        char        conversion_buffer[MAX_OCTET_STR_LEN + 1];
+        octet = &octet[octet_str_len - clamped_string_len];
+        strncpy(conversion_buffer, octet, clamped_string_len);
+        char* end = &conversion_buffer[clamped_string_len];
+        *end      = '\0';
+        return static_cast<Octet>(std::min(255ul, std::strtoul(conversion_buffer, &end, 10)));
+    }
+
+    /// Converts a string into an IPV4 address. The string must be in the format
+    /// "xxx.xxx.xxx.xxx" where each xxx is an value between 0 and 255 in base-10
+    /// (decimal). For example:
+    /// ```
+    ///     "192.168.0.10" // Four decimal octets
+    /// ```
+    /// @param[in] address      Address as a string.
+    /// @param[in] address_len  The length of the address argument.
+    /// @return IPV4 address
+    static inline Address addressFromString(const char* address, std::size_t address_len)
+    {
+        if (nullptr == address)
+        {
+            return Address();
+        }
+        std::size_t octet_index = 0;
+        Octet       octets[4]   = {0, 0, 0, 0};
+        const char* last        = address;
+
+        const char* i = std::strchr(last, '.');
+        while (i != nullptr && octet_index < 3 && i < &address[address_len])
+        {
+            octets[octet_index++] = octetFromBase10String(last, static_cast<std::size_t>(i - last));
+            last                  = i + 1;
+            i                     = std::strchr(last, '.');
+        }
+        if (octet_index < 4 && last < &address[address_len])
+        {
+            octets[octet_index++] = octetFromBase10String(last, address_len - static_cast<std::size_t>(last - address));
+        }
+        return Address(octets[0], octets[1], octets[2], octets[3]);
+    }
+
+    /// Converts a string into an IPV4 address. The string must be in the format
+    /// "xxx.xxx.xxx.xxx" where each xxx is an value between 0 and 255 in base-10
+    /// (decimal). For example:
+    /// ```
+    ///     "192.168.0.10" // Four decimal octets
+    /// ```
+    /// @param[in] address          Address as a null-terminated string.
+    /// @return IPV4 address
+    static inline Address addressFromString(const char* address)
+    {
+        if (nullptr == address)
+        {
+            return addressFromString(address, 0);
+        }
+        else
+        {
+            return addressFromString(address, std::strlen(address));
+        }
     }
 
 private:
