b92db328d2942a7e2c2158a994a16449bfc3c7f3
Sergei Shirokov <sergei.shirokov@zubax.com>
2024-04-18 16:17:40 +0300

    fix unit tests on cpp17+ #verification

--- test/unittest/memory_resource_mock.hpp
+++ test/unittest/memory_resource_mock.hpp
@@ -17,10 +17,13 @@ class MemoryResourceMock : public cetl::pmr::memory_resource
 {
 public:
     MOCK_METHOD(void*, do_allocate, (std::size_t, std::size_t));
-    MOCK_METHOD(std::size_t, do_max_size, (), (const, noexcept));
     MOCK_METHOD(void, do_deallocate, (void*, std::size_t, std::size_t));
     MOCK_METHOD(bool, do_is_equal, (const memory_resource&), (const, noexcept));
+
+#if (__cplusplus < CETL_CPP_STANDARD_17)
+    MOCK_METHOD(std::size_t, do_max_size, (), (const, noexcept));
     MOCK_METHOD(void*, do_reallocate, (void*, std::size_t, std::size_t, std::size_t));
+#endif
 
     void redirectExpectedCallsTo(cetl::pmr::memory_resource& mr)
     {
@@ -30,23 +33,22 @@ public:
             .WillRepeatedly([&mr](std::size_t size_bytes, std::size_t alignment) -> void* {
                 return mr.allocate(size_bytes, alignment);
             });
-
-        EXPECT_CALL(*this, do_max_size()).WillRepeatedly([&mr]() { return mr.max_size(); });
-
         EXPECT_CALL(*this, do_deallocate(_, _, _))
             .WillRepeatedly([&mr](void* p, std::size_t size_bytes, std::size_t alignment) {
                 mr.deallocate(p, size_bytes, alignment);
             });
-
         EXPECT_CALL(*this, do_is_equal(_)).WillRepeatedly([&mr](const memory_resource& rhs) {
             return mr.is_equal(rhs);
         });
 
+#if (__cplusplus < CETL_CPP_STANDARD_17)
+        EXPECT_CALL(*this, do_max_size()).WillRepeatedly([&mr]() { return mr.max_size(); });
         EXPECT_CALL(*this, do_reallocate(_, _, _, _))
             .WillRepeatedly(
                 [&mr](void* ptr, std::size_t old_size_bytes, std::size_t new_size_bytes, std::size_t alignment) {
                     return mr.reallocate(ptr, old_size_bytes, new_size_bytes, alignment);
                 });
+#endif
     }
 
 };  // MemoryResourceMock
