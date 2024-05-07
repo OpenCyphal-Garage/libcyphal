38b9c860e62b351d976f4803fc3b5f0cf3393e73
Sergei Shirokov <sergei.shirokov@zubax.com>
2024-05-07 16:01:14 +0300

    try #sonar

---
+++ test/unittest/sonar.cpp
@@ -0,0 +1,11 @@
+/// @copyright
+/// Copyright (C) OpenCyphal Development Team  <opencyphal.org>
+/// Copyright Amazon.com Inc. or its affiliates.
+/// SPDX-License-Identifier: MIT
+
+#include <libcyphal/types.hpp>
+
+int main()
+{
+    return 0;
+}
