471f8ac922fa0aa3268b2a2ff3c57a10ff4fdf3a
Sergei Shirokov <sergei.shirokov@zubax.com>
2024-09-18 19:46:21 +0300

    sonar fixes #verification #docs #sonar

--- include/libcyphal/presentation/presentation.hpp
+++ include/libcyphal/presentation/presentation.hpp
@@ -48,8 +48,12 @@ namespace detail
 
 /// Trait which determines whether the given type has `T::_traits_::HasFixedPortID` field.
 ///
+/// No Sonar "Reconsider this operator for `bool` operand'
+/// b/c we do need to check the existence of the field with help of `decltype` and `,` (comma) operator.
+///
 template <typename T>
-auto HasFixedPortIdTrait(bool dummy) -> decltype(std::decay_t<T>::_traits_::HasFixedPortID, std::true_type{});
+auto HasFixedPortIdTrait(bool dummy)                                           //
+    -> decltype(std::decay_t<T>::_traits_::HasFixedPortID, std::true_type{});  // NOSONAR cpp:S872
 template <typename>
 std::false_type HasFixedPortIdTrait(...);
 
